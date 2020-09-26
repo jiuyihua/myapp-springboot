719b71decde92f9c7d12f687a03614a46ad1999e
jiuyihua <jiuyi_huhua@163.com>
2020-09-26 16:00:16 +0800

    首次构建项目结构——增加邮件发送模块

---
+++ app-mail/src/main/java/com/app/appmail/AppMailApplication.java
@@ -0,0 +1,13 @@
+package com.app.appmail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppMailApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppMailApplication.class, args);
+    }
+
+}
---
+++ app-mail/src/test/java/com/app/appmail/AppMailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.appmail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppMailApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
